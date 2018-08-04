1f46c2a17908137bcd04c0b2b34f6347ace70a0b
Zhang Yi <loeyae@gmail.com>
2018-08-04 19:41:34 +0800

    update schema

--- cdspider/database/base/Uniquedb.py
+++ cdspider/database/base/Uniquedb.py
@@ -6,22 +6,22 @@
 """
 :author:  Zhang Yi <loeyae@gmail.com>
 :date:    2018-1-9 17:42:16
-:version: SVN: $Id: Uniquedb.py 2431 2018-07-31 01:30:53Z zhangyi $
 """
 
+from cdspider.libs import utils
+from . import Base
+
 {
-    'uniqueue': {
+    'unique': {
         'unid': str,         # unique str
-        'createtime': int,   # 创建时间
+        'ctime': int,        # 创建时间
     }
 }
 
 
-from cdspider.libs import utils
+class UniqueDB(Base):
 
-class UniqueDB(object):
-
-    def insert(self, obj, projectid, taskid, urlid, attachid, kwid, createtime):
+    def insert(self, obj, createtime):
         raise NotImplementedError
 
     def build(self, obj):
