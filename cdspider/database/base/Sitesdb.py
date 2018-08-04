c81b3845217713fb42b4029da924e44b9aefe7cc
Zhang Yi <loeyae@gmail.com>
2018-08-04 17:36:54 +0800

    update schema

--- cdspider/database/base/Sitesdb.py
+++ cdspider/database/base/Sitesdb.py
@@ -8,49 +8,34 @@
 :date:    2018-1-9 17:40:43
 :version: SVN: $Id: Sitedb.py 2119 2018-07-04 03:56:41Z zhangyi $
 """
+
+from . import Base
+
 # site schema
 {
-    'site': {
+    'sites': {
         'sid': int,          # site id
+        'pid': int,          # project id
         'name': str,         # site name
-        'status': int,       # site status
-        'projectid': int,    # project id
-        'rate': int,         # 更新频率
         'url': str,          # 基础 url
-        'stid': int,         # sitetype id
-        'script': str,       # 自定义handler
-        'base_request': str, # 基础请求配置
+        'status': int,       # site status
+        'rate': int,         # 更新频率
+        'comment': str,      # 站点描述
+        'scripts': str,      # 自定义handler
+        'type': str,         # 站点类型
         'main_process': str, # 主流程配置
         'sub_process': str,  # 子流程配置
-        'identify': str,     # 生成unique id的配置
-        'limb': int,         # 是否包含子集， 0 为否， 1为是
-        'domain': str,       # 站点域名
-        'createtime': int,   # 创建时间
-        'updatetime': int,   # 最后一次更新时间
-        'lastuid': int,      # 最后一次入队的 url id
-        'lastkwid': int,     # 最后一次入队的 keyword id
+        'unique': str,       # 生成unique id的配置
+        'ctime': int,        # 创建时间
+        'utime': int,        # 最后一次更新时间
         'creator': str,      # 创建人
         'updator': str,      # 最后一次更新人
     }
 }
 
-class SiteDB(object):
-
-    SITE_STATUS_INIT = 0
-    SITE_STATUS_ACTIVE = 1
-    SITE_STATUS_DISABLE = 2
-    SITE_STATUS_DELETED = 3
-
-    SITE_LIMB_TRUE = 1
-    SITE_LIMB_FALSE = 0
+class SitesDB(Base):
 
     def insert(self, obj={}):
-        raise NotImplementedError
-
-    def enable(self, id, where):
-        raise NotImplementedError
-
-    def enable_by_project(self, pid, where):
         raise NotImplementedError
 
     def disable(self, id, where):
@@ -74,11 +59,5 @@ class SiteDB(object):
     def get_detail(self, id):
         raise NotImplementedError
 
-    def get_new_list(self, id, projectid):
-        raise NotImplementedError
-
     def get_list(self, where = {}, select=None, **kwargs):
-        raise NotImplementedError
-
-    def get_max_id(self, projectid):
         raise NotImplementedError
