5c26208d442ff466cfee21d00f41d52f05420615
Zhang Yi <loeyae@gmail.com>
2018-08-04 20:36:17 +0800

    update schema

--- cdspider/database/base/__init__.py
+++ cdspider/database/base/__init__.py
@@ -3,18 +3,19 @@
 # see LICENSE for more details: http://www.apache.org/licenses/LICENSE-2.0.
 
 class Base(object):
-    
+
     STATUS_INIT = 0
     STATUS_ACTIVE = 1
     STATUS_DELETED = 9
 
 from .Admindb import AdminDB
-from .Projectdb import ProjectDB
-from .Sitedb import SiteDB
-from .Sitetypedb import SitetypeDB
-from .Urlsdb import UrlsDB
+from .AttachDatadb import AttachDataDB
 from .Attachmentdb import AttachmentDB
-from .Taskdb import TaskDB
+from .Commentsdb import CommentsDB
 from .Keywordsdb import KeywordsDB
+from .ParseRuledb import ParseRuleDB
+from .Projectsdb import ProjectsDB
+from .Sitesdb import SitesDB
+from .Taskdb import TaskDB
 from .Uniquedb import UniqueDB
-from .Resultdb import ResultDB
+from .Urlsdb import UrlsDB
