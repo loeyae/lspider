c251f6c490902d5be756d9c979521607a9685f0a
Zhang Yi <loeyae@gmail.com>
2018-08-04 20:32:29 +0800

    add ParseRuledb

--- cdspider/database/base/Urlsdb.py
+++ cdspider/database/base/Urlsdb.py
@@ -6,25 +6,23 @@
 """
 :author:  Zhang Yi <loeyae@gmail.com>
 :date:    2018-1-9 17:43:17
-:version: SVN: $Id: Urlsdb.py 2119 2018-07-04 03:56:41Z zhangyi $
 """
 
 {
     "urls": {
         'uid': int,           # url id
-        'title': str,         # title
-        'siteid': int,        # site id
+        'name': str,          # 名称
+        'sid': int,           # site id
         'url': str,           # url
         'rate': int,          # int
         'status': int,        # status
-        'base_request': str,  # 基础请求配置
         'main_process': str,  # 主流程配置
         'sub_process': str,   # 子流程配置
-        'identify': str,      # 生成unique id的配置
-        'createtime': int,    # create time
-        'updatetime': int,    # last update time
-        'creator': str,       # creator
-        'updator': str,       # updator
+        'unique': str,        # 生成unique id的配置
+        'ctime': int,         # create time
+        'utime': int,         # last update time
+        'creator': int,       # creator
+        'updator': int,       # last update updator
     }
 }
 
