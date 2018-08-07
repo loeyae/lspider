00a5988fb5cfade84795848cbaf0dab02c378062
Zhang Yi <loeyae@gmail.com>
2018-08-07 21:52:37 +0800

    fix bug

--- cdspider/handler/ProjectBaseHandler.py
+++ cdspider/handler/ProjectBaseHandler.py
@@ -7,6 +7,7 @@
 :author:  Zhang Yi <loeyae@gmail.com>
 :date:    2018-8-5 21:42:18
 """
+from tld import get_tld
 from cdspider.handler import BaseHandler, ResultTrait
 from cdspider.exceptions import *
 
@@ -29,10 +30,18 @@ class ProjectBaseHandler(BaseHandler, ResultTrait):
             typeinfo = self._typeinfo(final_url)
             self.list_to_item(final_url, data, typeinfo, page_source)
         elif self.mode == self.MODE_ITEM:
-            typeinfo = self._typeinfo(self.task.get('save', {}).get('parent_url', final_url))
+            typeinfo = self.parse_domain(final_url)
             unique = False if not 'unid' in self.task else self.task['unid']
             self.item_to_result(final_url, data, typeinfo, page_source, unique)
         elif self.mode == self.MODE_ATT:
-            typeinfo = self._typeinfo(self.task.get('save', {}).get('parent_url', final_url))
+            typeinfo = self.parse_domain(final_url)
             unique = False if not 'unid' in self.task else self.task['unid']
             self.attach_to_result(final_url, data, typeinfo, page_source, unique)
+
+    def parse_domain(self, final_url):
+        _url = final_url
+        parent_url = self.task.get('save', {}).get('parent_url', None)
+        if parent_url and get_tld(parent_url) == get_tld(final_url):
+            _url = parent_url
+        typeinfo = self._typeinfo(_url)
+        return typeinfo
