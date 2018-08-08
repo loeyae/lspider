a326be4a2ee70a7ac45b5904acc68c859d430201
Zhang Yi <loeyae@gmail.com>
2018-08-08 22:40:52 +0800

    add tools init

---
+++ cdspider/tools/__init__.py
@@ -0,0 +1,19 @@
+#-*- coding: utf-8 -*-
+# Licensed under the Apache License, Version 2.0 (the "License"),
+# see LICENSE for more details: http://www.apache.org/licenses/LICENSE-2.0.
+
+import abc
+import six
+import logging
+
+@six.add_metaclass(abc.ABCMeta)
+class Base(object):
+
+    def __init__(self, g):
+        self.g = g
+        self.logger = g['logger']
+        self.logger.setLevel(logging.DEBUG if g['debug'] else logging.WARN)
+
+    @abc.abstractmethod
+    def process(self):
+        pass
