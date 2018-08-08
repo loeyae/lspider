bffb527bbbd61ab184d980c9e61cb723aa65298b
Zhang Yi <loeyae@gmail.com>
2018-08-08 22:52:33 +0800

    add args to process

--- cdspider/tools/__init__.py
+++ cdspider/tools/__init__.py
@@ -15,5 +15,5 @@ class Base(object):
         self.logger.setLevel(logging.DEBUG if g['debug'] else logging.WARN)
 
     @abc.abstractmethod
-    def process(self):
+    def process(self， *args, **kwargs):
         pass
