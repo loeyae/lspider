7bb05afccfc8b6e6ba9f8558be4bc119631e067e
Zhang Yi <loeyae@gmail.com>
2018-08-09 19:26:56 +0800

    fix incr_data bug

--- cdspider/libs/url_builder.py
+++ cdspider/libs/url_builder.py
@@ -268,9 +268,10 @@ class UrlBuilder():
                         incr_data[i]['value'] = 1
                     if not 'step' in incr_data[i] or not incr_data[i]['step']:
                         incr_data[i]['step'] = 1
-                    incr_data[i]['base_page'] = int(incr_data[i]['value'])
-                    if abs(int(incr_data[i].get('step', 1))) > 1 and incr_data[i]['base_page'] > 0:
-                        incr_data[i]['value'] = (int(incr_data[i].get('value', 0)) - 1) * abs(int(incr_data[i].get('step', 1)))
+                    if not 'base_page' in incr_data[i]
+                        incr_data[i]['base_page'] = int(incr_data[i]['value'])
+                    if int(incr_data[i]['value']) > incr_data[i]['base_page']:
+                        incr_data[i]['value'] = int(incr_data[i].get('value', 0)) - int(incr_data[i].get('step', 1))
                     incr_data[i].setdefault('type', 'url')
                 save['incr_data'] = incr_data
             for i in range(len(save['incr_data'])):
