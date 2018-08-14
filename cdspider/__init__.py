fae1926319c8dcd61a387cf27405254363081bca
Zhang Yi <loeyae@gmail.com>
2018-08-14 19:52:46 +0800

    add Component

--- cdspider/__init__.py
+++ cdspider/__init__.py
@@ -2,3 +2,35 @@
 # Licensed under the Apache License, Version 2.0 (the "License"),
 # see LICENSE for more details: http://www.apache.org/licenses/LICENSE-2.0.
 
+import logging
+
+class Component(Object):
+
+    def __init__(self, logger, log_level)
+        self.logger = logger or logging.getLogger('root')
+        self.logger.setLevel(log_level)
+        self.log_level = log_level
+
+    def debug(self, message, *args, **kwargs):
+        if self.log_level >= loggin.DEBUG:
+            self.logger.debug(message, *args, **kwargs)
+
+    def info(self, message, *args, **kwargs):
+        if self.log_level >= loggin.INFO:
+            self.logger.info(message, *args, **kwargs)
+
+    def warn(self, message, *args, **kwargs):
+        if self.log_level >= loggin.WARN:
+            self.logger.warn(message, *args, **kwargs)
+
+    def error(self, message, *args, **kwargs):
+        if self.log_level >= loggin.ERROR:
+            self.logger.error(message, *args, **kwargs)
+
+    def fatal(self, message, *args, **kwargs):
+        if self.log_level >= loggin.FATAL:
+            self.logger.fatal(message, *args, **kwargs)
+
+    def critical(self, message, *args, **kwargs):
+        if self.log_level >= loggin.CRITICAL:
+            self.logger.critical(message, *args, **kwargs)
