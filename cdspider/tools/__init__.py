4350f09e85640948a377b48003336ac87e8f4aff
Zhang Yi <loeyae@gmail.com>
2018-08-14 08:53:22 +0800

    add notice

--- cdspider/tools/__init__.py
+++ cdspider/tools/__init__.py
@@ -2,6 +2,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License"),
 # see LICENSE for more details: http://www.apache.org/licenses/LICENSE-2.0.
 
+import sys
 import abc
 import six
 import logging
@@ -18,3 +19,20 @@ class Base(object):
     @abc.abstractmethod
     def process(self, *args, **kwargs):
         pass
+
+    def notice(self, message, data = None, checked = True):
+        """
+        notice
+        """
+        print('=============================================')
+        print(str(message))
+        if data:
+            print(str(data))
+        print('=============================================')
+        if checked:
+            x = None
+            while x not in ('y', 'Y', 'n', 'N'):
+                x = input('Proceed (y[Y]/n[N]):')
+            if x in ('N', 'n'):
+                print('Quit')
+                sys.exit(0)
