5926f063a8124da73a1da3fd836b578bc825ddaf
Zhang Yi <loeyae@gmail.com>
2018-08-15 07:55:32 +0800

    fix bug

--- cdspider/spider/Spider.py
+++ cdspider/spider/Spider.py
@@ -148,6 +148,7 @@ class Spider(Component):
         finally:
             if not return_result:
                 handler.finish()
+            del handler
             self.info("Spider fetch end" )
             if return_result:
                 return return_data
