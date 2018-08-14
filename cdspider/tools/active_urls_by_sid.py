c69f46fba30654fe404faf7e9f8060633bce444d
Zhang Yi <loeyae@gmail.com>
2018-08-14 09:46:19 +0800

    fix bug

--- cdspider/tools/active_urls_by_sid.py
+++ cdspider/tools/active_urls_by_sid.py
@@ -16,14 +16,17 @@ class active_urls_by_sid(Base):
         site = self.g['db']['SitesDB'].get_detail(sid)
         self.broken('Site: %s not exists' % sid, site)
         self.notice('Selected Site Info:', site)
+        uid = 0
         while True:
             i = 0
-            for item in self.g['db']['UrlsDB'].get_list(where={'sid': sid,'status': 0}):
+            for item in self.g['db']['UrlsDB'].get_new_list(uid, sid, where={'status': 0}):
                 d={}
                 d['status']=1
                 d['uid'] = item['uid']
                 self.logger.info("push status_queue data: %s" %  str(d))
                 self.g['queue']['status_queue'].put_nowait(d)
                 i += 1
+                if item['uid'] > uid:
+                    uid = item['uid']
             if i < 1:
                 return
