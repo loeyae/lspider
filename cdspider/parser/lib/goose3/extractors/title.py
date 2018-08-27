0b9f3180052338e72a9aabd67916c64a820a4cda
Zhang Yi <loeyae@gmail.com>
2018-08-27 19:44:58 +0800

    fix bug

--- cdspider/parser/lib/goose3/extractors/title.py
+++ cdspider/parser/lib/goose3/extractors/title.py
@@ -198,6 +198,10 @@ class TitleExtractor(BaseExtractor):
                     if len(ts) == 1 and ts[0].strip():
                         title = ts[0].strip()
                         return {'raw_title': title, 'clean_title': self.clean_title(title)}
+                    ts = ele.xpath('font/text()')
+                    if len(ts) == 1 and ts[0].strip():
+                        title = ts[0].strip()
+                        return {'raw_title': title, 'clean_title': self.clean_title(title)}
                     break
 
             # otherwise use the title meta
