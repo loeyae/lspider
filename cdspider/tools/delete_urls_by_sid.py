3e192970c052dbaa285b3856484f4cc888a8162f
Zhang Yi <loeyae@gmail.com>
2018-08-31 10:42:57 +0800

    fix bug

---
+++ cdspider/tools/delete_urls_by_sid.py
@@ -0,0 +1,35 @@
+#-*- coding: utf-8 -*-
+
+'''
+Created on 2018年6月20日
+
+@author: Wang Fengwei
+'''
+from cdspider.tools import Base
+
+class delete_urls_by_sid(Base):
+
+    def process(self, *args):
+        assert len(args) > 0, 'Please input sid'
+        sid = int(args[0])
+        self.broken('Site not exists', sid)
+        site = self.g['db']['SitesDB'].get_detail(sid)
+        self.broken('Site: %s not exists' % sid, site)
+        self.notice('Selected Site Info:', site)
+        uid = 0
+        if len(args) > 1:
+            uid = int(args[1])
+        UrlsDB = self.g['db']['UrlsDB']
+        while True:
+            i = 0
+            for item in UrlsDB.get_new_list(uid, sid, where={'status': {"$in": [UrlsDB.STATUS_INIT, UrlsDB.STATUS_ACTIVE]}}):
+                d={}
+                d['status']= UrlsDB.STATUS_DELETED
+                d['uid'] = item['uid']
+                self.logger.info("push status_queue data: %s" %  str(d))
+                self.g['queue']['status_queue'].put_nowait(d)
+                i += 1
+                if item['uid'] > uid:
+                    uid = item['uid']
+            if i < 1:
+                return
