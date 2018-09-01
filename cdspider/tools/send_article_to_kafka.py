24996458be99e0b268701e5c8a7a5dfb3a2919a8
Zhang Yi <loeyae@gmail.com>
2018-09-01 16:24:23 +0800

    fix bug

---
+++ cdspider/tools/send_article_to_kafka.py
@@ -0,0 +1,37 @@
+#-*- coding: utf-8 -*-
+
+'''
+Created on 2018年8月9日
+
+@author: Wang Fengwei
+'''
+import json
+from cdspider.tools import Base
+import time
+
+class send_article_to_kafka(Base):
+
+    def process(self):
+        assert len(args) > 0, 'Please input where'
+        where = json.loads(args[0])
+        created = 0
+        if len(args) > 1:
+            created = int(created)
+        sum=0
+        acid = '0'
+        where['acid'] = {"$gt": acid}
+        self.notice('Where Info:', where)
+        while True:
+            n=0
+            for item in self.g['db']['ArticlesDB'].get_list(created, where=where):
+                n=n+1
+                sum=sum+1
+                d={}
+                d['rid']=item['rid']
+                d['on_sync']='publicOpinionCustomeNS:publicOpinionCustomeTB001'
+                self.logger.info("import status_queue data: %s" %  str(d))
+                acid=item['acid']
+                self.g['queue']['result2kafka'].put_nowait(d)
+            if n==0:
+                break
+        print(str(sum))
