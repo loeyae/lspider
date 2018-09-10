4e13d32a4af7bf5c37a86c41315bab18a0c04970
wangfengwei <wangfengwei0922@163.com>
2018-09-10 13:25:04 +0800

    1

--- cdspider/message_queue/KafkaQueue.py
+++ cdspider/message_queue/KafkaQueue.py
@@ -102,7 +102,7 @@ class KafkaQueue(CDBaseQueue):
         （obj>>json格式）
         """
         self.logger.info('send kafka starting ....')
-        producer = self.connect.get_producer(linger_ms=1,sync=False)
+        producer = self.connect.get_producer(linger_ms=0.1,sync=False)
         obj=json.dumps(obj)
         obj=obj.encode(encoding='utf_8')
         producer.produce(obj)
