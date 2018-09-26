7150604940c07b705f2a803bb1c330ad7cfd01ca
Zhang Yi <loeyae@gmail.com>
2018-09-26 16:13:55 +0800

    update kafka setting

--- cdspider/worker/__init__.py
+++ cdspider/worker/__init__.py
@@ -77,4 +77,4 @@ class BaseWorker(Component):
 
 from .result_worker import ResultWorker
 from .exc_worker import ExcWorker
-from .insert_kafka_worker import SyncKafkaWorker
+from .sync_kafka_worker import SyncKafkaWorker
