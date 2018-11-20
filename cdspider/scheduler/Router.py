d348992bbe8c20e04bf51411b5f0d232f45351cc
Zhang Yi <loeyae@gmail.com>
2018-11-20 21:26:46 +0800

    add schedule

--- cdspider/scheduler/Router.py
+++ cdspider/scheduler/Router.py
@@ -10,6 +10,8 @@ import time
 import logging
 from . import BaseScheduler
 from cdspider.exceptions import *
+from cdspider.libs.constants import *
+from cdspider.libs.utils import get_object
 
 class Router(BaseScheduler):
     """
@@ -24,88 +26,32 @@ class Router(BaseScheduler):
         self.outqueue = self.queue["scheduler2task"]
         self.mode = mode
         self.interval = 5
-        self.projects = set()
         self._check_time = None
-        self._check_projects()
 
-    def _check_projects(self):
-        self.info("%s check_projects starting..." % self.__class__.__name__)
-        projectid = 0
-        while True:
-            projects = self.db['ProjectsDB'].get_list(where=[('pid', "$gt", projectid), ("status", self.db['ProjectsDB'].STATUS_ACTIVE)])
-            i = 0
-            for project in projects:
-                self.debug("%s check_projects project: %s " % (self.__class__.__name__, str(project)))
-                self.projects.add(project['pid'])
-                if project['pid'] > projectid:
-                    projectid = project['pid']
-                i += 1
-            if i == 0:
-                self.debug("%s check_projects no projects" % self.__class__.__name__)
-                break
-        self._check_time = int(time.time())
-        self.info("%s check_projects end" % self.__class__.__name__)
+    def valid(self):
+        if self.outqueue.qsize() > 0:
+            self.debug("scheduler2task is running")
+            return False
+        return True
 
     def schedule(self, message = None):
-        if self.outqueue.qsize() > 0:
-            self.debug("%s outqueue is not empty" % self.__class__.__name__)
-            return
-        self.info("%s schedule starting..." % self.__class__.__name__)
-        if self._check_time - int(time.time()) > 3600:
-            self._check_projects()
-        if self.mode == self.MODE_PROJECT:
-            for pid in self.projects:
-                self.debug("push pid: %s into queue" % pid)
-                self.outqueue.put_nowait({"pid": pid})
-        elif self.mode == self.MODE_SITE:
-            for pid in self.projects:
-                sid = 0
-                while True:
-                    sites = self.db['SitesDB'].get_new_list(sid, pid, where = {"status": self.db['SitesDB'].STATUS_ACTIVE}, select=["sid"])
-                    i = 0
-                    for site in sites:
-                        sid = site['sid']
-                        self.debug("push sid: %s into queue" % sid)
-                        self.outqueue.put_nowait({"pid": pid, "sid": sid})
-                        i += 1
-                    if i == 0:
-                        break
-        elif self.mode == self.MODE_ITEM:
-            for pid in self.projects:
-                uid = 0
-                while True:
-                    urls = self.db['UrlsDB'].get_new_list_by_pid(uid, pid, where = {"status": self.db['UrlsDB'].STATUS_ACTIVE}, select=["uid"])
-                    i = 0
-                    for url in urls:
-                        uid = url['uid']
-                        self.debug("push uid: %s into queue" % uid)
-                        self.outqueue.put_nowait({"pid": pid, "uid": uid})
-                        i += 1
-                    if i == 0:
-                        break
-                aid = 0
-                while True:
-                    attachments = self.db['AttachmentDB'].get_new_list_by_pid(uid, pid, where = {"status": self.db['AttachmentDB'].STATUS_ACTIVE}, select=['aid'])
-                    i = 0
-                    for attachment in attachments:
-                        aid = attachment['aid']
-                        self.debug("push aid: %s into queue" % aid)
-                        self.outqueue.put_nowait({"pid": pid, "aid": aid})
-                        i += 1
-                    if i == 0:
-                        break
-                crid = 0
-                while True:
-                    channels = self.db['ChannelRulesDB'].get_new_list_by_pid(uid, pid, where = {"status": self.db['ChannelRulesDB'].STATUS_ACTIVE}, select=['crid'])
-                    i = 0
-                    for channel in channels:
-                        crid = channel['crid']
-                        self.debug("push crid: %s into queue" % crid)
-                        self.outqueue.put_nowait({"pid": pid, "crid": crid})
-                        i += 1
-                    if i == 0:
-                        break
-        self.info("%s schedule end" % self.__class__.__name__)
+        self.info("%s route starting..." % self.__class__.__name__)
+        for key, name in HANDLER_MODE_HANDLER_MAPPING.items():
+            handler = get_object("cdspider.handler.%s" % name)(self.ctx, None)
+            save = {}
+            while True:
+                has_item = False
+                for item in handler.route(self.mode, save):
+                    has_item = True
+                    message = {
+                        "mode": self.mode,
+                        "h-mode": key,
+                        "item": item,
+                    }
+                    self.outqueue.put_nowait(message)
+                if not has_item:
+                    break
+        self.info("%s route end" % self.__class__.__name__)
 
     def xmlrpc_run(self, port=23333, bind='127.0.0.1'):
         import umsgpack
