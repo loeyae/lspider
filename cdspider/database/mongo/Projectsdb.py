99b6c84a1d972c0cb12694a758d88f2c133b4575
Zhang Yi <loeyae@gmail.com>
2018-11-21 23:15:03 +0800

    update route

--- cdspider/database/mongo/Projectsdb.py
+++ cdspider/database/mongo/Projectsdb.py
@@ -10,7 +10,7 @@
 """
 import time
 import pymongo
-from cdspider.database.base import ProjectsDB as BaseProjectsDB
+from cdsuuider.database.base import ProjectsDB as BaseProjectsDB
 from .Mongo import Mongo
 
 class ProjectsDB(Mongo, BaseProjectsDB):
@@ -23,43 +23,50 @@ class ProjectsDB(Mongo, BaseProjectsDB):
         super(ProjectsDB, self).__init__(connector, table = table, **kwargs)
         collection = self._db.get_collection(self.table)
         indexes = collection.index_information()
-        if not 'pid' in indexes:
-            collection.create_index('pid', unique=True, name='pid')
+        if not 'uuid' in indexes:
+            collection.create_index('uuid', unique=True, name='uuid')
         if not 'status' in indexes:
             collection.create_index('status', name='status')
         if not 'ctime' in indexes:
             collection.create_index('ctime', name='ctime')
 
     def get_detail(self, id):
-        return self.get(where={'pid': int(id)})
+        return self.get(where={'uuid': int(id)})
 
     def insert(self, obj):
-        obj['pid'] = self._get_increment(self.incr_key)
+        obj['uuid'] = self._get_increment(self.incr_key)
         obj.setdefault('status', self.STATUS_INIT)
         obj.setdefault('ctime', int(time.time()))
         obj.setdefault('utime', 0)
         super(ProjectsDB, self).insert(setting=obj)
-        return obj['pid']
+        return obj['uuid']
 
     def update(self, id, obj):
         obj['utime'] = int(time.time())
-        return super(ProjectsDB, self).update(setting=obj, where={"pid": int(id)}, multi=False)
+        return super(ProjectsDB, self).update(setting=obj, where={"uuid": int(id)}, multi=False)
 
     def active(self, id):
         return super(ProjectsDB, self).update(setting={"status": self.STATUS_ACTIVE},
-                where={"pid": int(id)}, multi=False)
+                where={"uuid": int(id)}, multi=False)
 
     def disable(self, id):
         return super(ProjectsDB, self).update(setting={"status": self.STATUS_INIT},
-                where={"pid": int(id)}, multi=False)
+                where={"uuid": int(id)}, multi=False)
 
     def delete(self, id):
         return super(ProjectsDB, self).update(setting={"status": self.STATUS_DELETED},
-                where={"pid": int(id)}, multi=False)
+                where={"uuid": int(id)}, multi=False)
 
     def get_count(self, where = {}, select = None, **kwargs):
         return self.count(where=where, select=select, **kwargs)
 
     def get_list(self, where = {}, select = None, **kwargs):
-        kwargs.setdefault('sort', [('pid', 1)])
+        kwargs.setdefault('sort', [('uuid', 1)])
+        return self.find(where=where, select=select, **kwargs)
+
+    def get_new_list(self, id, where = {}, select = None, **kwargs):
+        kwargs.setdefault('sort', [('uuid', 1)])
+        if not where:
+            where == {}
+        where['uuid'] = {"$gt": id}
         return self.find(where=where, select=select, **kwargs)
