42da9fad018fc766b78aa628d0e1b0972b0ae7ef
Zhang Yi <loeyae@gmail.com>
2018-12-02 15:59:24 +0800

    add CommentHandler

---
+++ cdspider/handler/InteractHandler.py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+# Licensed under the Apache License, Version 2.0 (the "License"),
+# see LICENSE for more details: http://www.apache.org/licenses/LICENSE-2.0.
+
+"""
+:author:  Zhang Yi <loeyae@gmail.com>
+:date:    2018-12-2 15:16:45
+"""
+import copy
+import time
+from . import BaseHandler
+from urllib.parse import urljoin, urlparse, urlunparse
+from cdspider.database.base import *
+from cdspider.libs import utils
+from cdspider.libs.constants import *
+from cdspider.parser import CustomParser
+
+
+class InteractHandler(BaseHandler):
+    """
+    interact handler
+    """
+    pass
