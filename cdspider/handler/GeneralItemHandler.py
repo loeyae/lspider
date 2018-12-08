5432ea3df11497532ca8e95b39105ede80632741
Zhang Yi <loeyae@gmail.com>
2018-12-08 11:00:31 +0800

    fix bug

--- cdspider/handler/GeneralItemHandler.py
+++ cdspider/handler/GeneralItemHandler.py
@@ -8,6 +8,7 @@
 """
 import time
 import copy
+import traceback
 from . import BaseHandler
 from cdspider.database.base import *
 from cdspider.libs import utils
@@ -18,19 +19,33 @@ from cdspider.parser.lib import TimeParser
 class GeneralItemHandler(BaseHandler):
     """
     general item handler
+    :property task 爬虫任务信息 {"mode": "item", "rid": Article uuid}
+                   当测试该handler，数据应为 {"mode": "item", "url": url, "detailRule": 详情规则，参考详情规则}
     """
 
     def get_scripts(self):
+        """
+        获取自定义脚本
+        """
         rule = self.match_rule()
         return rule.get("scripts", None)
 
     def init_process(self):
+        """
+        初始化爬虫流程
+        :output self.process {"request": 请求设置, "parse": 解析规则, "paging": 分页规则}
+        """
         self.process = self.match_rule()
         if 'paging' in self.process and self.process['paging']:
             self.process['paging']['url'] = 'base_url'
 
     def match_rule(self):
+        """
+        匹配详情页规则
+        """
+        #优先获取task中详情规则
         parse_rule = self.task.get("detailRule", {})
+        #根据task中的rid获取文章信息
         rid = self.task.get('rid', None)
         if rid:
             article = self.db['ArticlesDB'].get_detail(rid, select=['url', 'crawlinfo'])
@@ -45,32 +60,65 @@ class GeneralItemHandler(BaseHandler):
         if not url:
             raise CDSpiderHandlerError("url not exists")
         if not parse_rule:
+            '''
+            task中不存在详情规则，根据域名匹配规则库中的规则
+            '''
             subdomain, domain = utils.parse_domain(url)
             if subdomain:
+                '''
+                优先获取子域名对应的规则
+                '''
                 parserule_list = self.db['ParseRuleDB'].get_list_by_subdomain(subdomain)
                 for item in parserule_list:
                     if not parse_rule:
+                        '''
+                        将第一条规则选择为返回的默认值
+                        '''
                         parse_rule = item
                     if  'urlPattern' in item and item['urlPattern']:
+                        '''
+                        如果规则中存在url匹配规则，则进行url匹配规则验证
+                        '''
                         u = utils.preg(url, item['urlPattern'])
                         if u:
                             return item
             else:
+                '''
+                获取域名对应的规则
+                '''
                 parserule_list = self.db['ParseRuleDB'].get_list_by_domain(domain)
                 for item in parserule_list:
                     if not parse_rule:
+                        '''
+                        将第一条规则选择为返回的默认值
+                        '''
                         parse_rule = item
                     if  'urlPattern' in item and item['urlPattern']:
+                        '''
+                        如果规则中存在url匹配规则，则进行url匹配规则验证
+                        '''
                         u = utils.preg(url, item['urlPattern'])
                         if u:
                             return item
         return parse_rule
 
     def run_parse(self, rule):
+        """
+        文章解析
+        :param rule 解析规则
+        :input self.response 爬虫结果 {"last_source": 最后一次抓取到的源码, "final_url": 最后一次请求的url}
+        :output self.response {"parsed": 解析结果}
+        """
         parser = ItemParser(source=self.response['last_source'], ruleset=copy.deepcopy(rule), log_level=self.log_level, url=self.response['final_url'])
         self.response['parsed'] = parser.parse()
 
     def _build_crawl_info(self, final_url):
+        """
+        构造爬虫log信息
+        :param final_url 请求的url
+        :input self.task 爬虫任务信息
+        :input self.page 当前的页码
+        """
         if not 'final_url' in self.task['crawlinfo']:
             self.task['crawlinfo']['final_url'] = {str(self.page): final_url}
         else:
@@ -80,8 +128,19 @@ class GeneralItemHandler(BaseHandler):
         self.task['crawlinfo']['page'] = self.page
 
     def _build_result_info(self, **kwargs):
+        """
+        构造文章数据
+        :param result 解析到的文章信息 {"title": 标题, "author": 作者, "pubtime": 发布时间, "content": 内容}
+        :param final_url 请求的url
+        :param typeinfo 域名信息 {'domain': 一级域名, 'subdomain': 子域名}
+        :param crawlinfo 爬虫信息
+        :param unid 文章唯一索引
+        :param ctime 抓取时间
+        :param status 状态
+        """
         now = int(time.time())
         result = kwargs.pop('result')
+        #格式化发布时间
         pubtime = TimeParser.timeformat(str(result.pop('pubtime', '')))
         if pubtime and pubtime > now:
             pubtime = now
@@ -96,6 +155,9 @@ class GeneralItemHandler(BaseHandler):
             'crawlinfo': kwargs.get('crawlinfo')
         }
         if all((r['title'], r['author'], r['content'], r['pubtime'])):
+            '''
+            判断文章是否解析完全
+            '''
             r['status'] = ArticlesDB.STATUS_PARSED
         if "unid" in kwargs:
             r['acid'] = kwargs['unid']                                         # unique str
@@ -106,40 +168,50 @@ class GeneralItemHandler(BaseHandler):
             r['subdomain'] = kwargs.get("typeinfo", {}).get('subdomain', None),    # 站点域名
         return r
 
-    def _domain_info(self, url):
-        subdomain, domain = utils.parse_domain(url)
-        if not subdomain:
-            subdomain = 'www'
-        return "%s.%s" % (subdomain, domain), domain
-
-    def _typeinfo(self, url):
-        subdomain, domain = self._domain_info(url)
-        return {"domain": domain, "subdomain": subdomain}
-
     def run_result(self, save):
+        """
+        爬虫结果处理
+        :param save 保存的上下文信息
+        """
         self._build_crawl_info(final_url=self.response['final_url'])
         if self.response['parsed']:
-            typeinfo = self._typeinfo(self.response['final_url'])
+            typeinfo = utils.typeinfo(self.response['final_url'])
             self.result2db(save, typeinfo)
             self.result2attach(save, **typeinfo)
 
     def result2db(self, save, typeinfo):
+        """
+        详情解析结果入库
+        :param save 保存的上下文信息
+        :param typeinfo 域名信息
+        """
         result_id = self.task.get("rid", None)
         if not result_id:
+            '''
+            如果任务中没有文章id，则生成文章唯一索引，并判断是否已经存在。
+            如果文章已经存在，则修改原数据，如果不存在，则新增数据
+            '''
             if self.testing_mode:
+                '''
+                testing_mode打开时，数据不入库
+                '''
                 inserted, unid = (True, {"acid": "testing_mode", "ctime": self.crawl_id})
                 self.debug("%s test mode: %s" % (self.__class__.__name__, unid))
             else:
+                #生成文章唯一索引并验证是否已存在
                 inserted, unid = self.db['UniqueDB'].insert(self.get_unique_setting(self.response['final_url'], self.response['parsed']), ctime)
                 self.debug("%s on_result unique: %s @ %s" % (self.__class__.__name__, str(inserted), str(unid)))
+            #格式化文章信息
             result = self._build_result_info(final_url=self.response['final_url'], typeinfo=typeinfo, result=self.response['parsed'], crawlinfo=self.task['crawlinfo'], **unid)
             if self.testing_mode:
+                '''
+                testing_mode打开时，数据不入库
+                '''
                 result_id = 'testing_mode'
                 self.debug("%s on_result: %s" % (self.__class__.__name__, result))
             else:
                 self.debug("%s on_result formated data: %s" % (self.__class__.__name__, str(result)))
                 if inserted:
-#                    result['parentid'] = parentid
                     result_id = self.db['ArticlesDB'].insert(result)
                 else:
                     item = self.db['ArticlesDB'].get_detail_by_unid(**unid)
@@ -148,15 +220,26 @@ class GeneralItemHandler(BaseHandler):
             self.task['rid'] = result_id
         else:
             if self.page == 1:
+                '''
+                对于已存在的文章，如果是第一页，则更新所有解析到的内容
+                否则只追加content的内容
+                '''
+                #格式化文章信息
                 result = self._build_result_info(final_url=self.response['final_url'], typeinfo=typeinfo, result=self.response['parsed'], crawlinfo=self.task['crawlinfo'])
 
                 if self.testing_mode:
+                    '''
+                    testing_mode打开时，数据不入库
+                    '''
                     self.debug("%s on_result: %s" % (self.__class__.__name__, result))
                 else:
                     self.debug("%s on_result formated data: %s" % (self.__class__.__name__, str(result)))
                     self.db['ArticlesDB'].update(result_id, result)
             else:
                 if self.testing_mode:
+                    '''
+                    testing_mode打开时，数据不入库
+                    '''
                     self.debug("%s on_result: %s" % (self.__class__.__name__, self.response['parsed']))
                 else:
                     result = self.db['ArticlesDB'].get_detail(result_id)
@@ -169,68 +252,120 @@ class GeneralItemHandler(BaseHandler):
             raise CDSpiderDBError("Result insert failed")
 
     def finish(self):
+        """
+        记录抓取日志
+        """
         super(GeneralItemHandler, self).finish()
         if self.task.get('rid') and self.task.get('crawlinfo') and not self.testing_mode:
             self.db['ArticlesDB'].update(self.task['rid'], {"crawlinfo": self.task['crawlinfo']})
 
     def result2attach(self, save, domain, subdomain=None):
+        """
+        根据详情页生成附加任务
+        :param save 传递的上下文信息
+        :param domain 域名
+        :param subdomain 子域名
+        """
         if self.page != 1:
+            '''
+            只在第一页时执行
+            '''
             return
         self.result2comment(save, domain, subdomain)
         self.result2interact(save, domain, subdomain)
 
     def result2comment(self, save, domain, subdomain = None):
+        """
+        根据详情页生成评论任务
+        :param save 传递的上下文信息
+        :param domain 域名
+        :param subdomain 子域名
+        """
+        def build_task(rule):
+            try:
+                url = self.build_attach_url(rule)
+                if url:
+                    '''
+                    根据规则生成出任务url，则为成功
+                    '''
+                    cid = self.build_comment_task(url, rule)
+                    self.task['crawlinfo']['commentRule'] = rule['uuid']
+                    self.task['crawlinfo']['commentTaskId'] = cid
+                    self.debug("%s new comment task: %s" % (self.__class__.__name__, str(cid)))
+                    return cid
+                return False
+            except:
+                self.error(traceback.format_exc())
+                return False
+        #通过子域名获取评论任务
         ruleset = self.db['CommentRuleDB'].get_list_by_subdomain(subdomain, where={"status": self.db['CommentRuleDB'].STATUS_ACTIVE})
         for rule in ruleset:
-            url = self.build_attach_url(rule)
-            if url:
-                cid = self.build_comment_task(url, rule)
-                self.task['crawlinfo']['commentRule'] = rule['uuid']
-                self.task['crawlinfo']['commentTaskId'] = cid
-                self.debug("%s new comment task: %s" % (self.__class__.__name__, str(cid)))
+            if build_task(rule):
                 return
+        #通过域名获取评论任务
         ruleset = self.db['CommentRuleDB'].get_list_by_domain(domain, where={"status": self.db['CommentRuleDB'].STATUS_ACTIVE})
         for rule in ruleset:
-            url = self.build_attach_url(rule)
-            if url:
-                cid = self.build_comment_task(url, rule)
-                self.task['crawlinfo']['commentRule'] = rule['uuid']
-                self.task['crawlinfo']['commentTaskId'] = cid
-                self.debug("%s new comment task: %s" % (self.__class__.__name__, str(cid)))
+            if build_task(rule):
                 return
 
     def result2interact(self, save, domain, subdomain = None):
+        """
+        根据详情页生成互动数任务
+        :param save 传递的上下文信息
+        :param domain 域名
+        :param subdomain 子域名
+        """
+        def buid_task(rule):
+            try:
+                url = self.build_attach_url(rule)
+                if url:
+                    '''
+                    根据规则生成出任务url，则为成功
+                    '''
+                    cid = self.build_interact_task(url, rule)
+                    self.task['crawlinfo']['interactRule'] = rule['uuid']
+                    self.task['crawlinfo']['interactTaskId'] = cid
+                    if 'interactRuleList' in  self.task['crawlinfo']:
+                         self.task['crawlinfo']['interactRuleList'][str(rule['uuid'])] = cid
+                    else:
+                        self.task['crawlinfo']['interactRuleList'] = {str(rule['uuid']): cid}
+                    self.debug("%s new interact task: %s" % (self.__class__.__name__, str(cid)))
+            except:
+                self.error(traceback.format_exc())
+        #通过子域名获取互动数任务
         ruleset = self.db['AttachmentDB'].get_list_by_subdomain(subdomain, where={"status": self.db['AttachmentDB'].STATUS_ACTIVE})
         for rule in ruleset:
-            url = self.build_attach_url(rule)
-            if url:
-                cid = self.build_interact_task(url, rule)
-                self.task['crawlinfo']['interactRule'] = rule['uuid']
-                self.task['crawlinfo']['interactTaskId'] = cid
-                self.debug("%s new interact task: %s" % (self.__class__.__name__, str(cid)))
-                return
+            buid_task(rule)
+        #通过域名获取互动数任务
         ruleset = self.db['AttachmentDB'].get_list_by_domain(domain, where={"status": self.db['AttachmentDB'].STATUS_ACTIVE})
         for rule in ruleset:
-            url = self.build_attach_url(rule)
-            if url:
-                cid = self.build_interact_task(url, rule)
-                self.task['crawlinfo']['interactRule'] = rule['uuid']
-                self.task['crawlinfo']['interactTaskId'] = cid
-                self.debug("%s new interact task: %s" % (self.__class__.__name__, str(cid)))
-                return
+            buid_task(rule)
 
     def build_attach_url(self, rule):
+        """
+        根据规则构造附加任务url
+        :param rule 附加任务url生成规则
+        """
         if 'preparse' in rule and rule['preparse']:
-            parsed = self.attach_preparse(rule['preparse'].get('parse', None))
-            if parsed:
-                urlrule = rule['preparse'].get('url', None)
-                if urlrule:
-                    if urlrule['base'] == 'parent_url':
-                        urlrule['base'] = self.response['final_url']
-                    return utils.build_url_by_rule(urlrule, parsed)
+            #根据解析规则匹配解析内容
+            rule = rule['preparse'].get('parse', None)
+            parsed = {}
+            if rule:
+                parsed = self.attach_preparse(rule)
+            urlrule = rule['preparse'].get('url', {})
+            if urlrule:
+                #格式化url设置，将parent_rul替换为详情页url
+                if urlrule['base'] == 'parent_url':
+                    urlrule['base'] = self.response['final_url']
+            return utils.build_url_by_rule(urlrule, parsed)
         return None
 
     def build_comment_task(self, url, rule):
+        """
+        构造评论任务
+        :param url taks url
+        :param rule 评论任务规则
+        """
         task = {
             'mode': HANDLER_MODE_COMMENT,                           # handler mode
             'pid': self.task['crawlinfo'].get('pid', 0),            # project id
@@ -244,26 +379,65 @@ class GeneralItemHandler(BaseHandler):
         }
         self.debug("%s build comment task: %s" % (self.__class__.__name__, str(task)))
         if not self.testing_mode:
+            '''
+            testing_mode打开时，数据不入库
+            '''
+            return self.db['SpiderTaskDB'].insert(task)
+        else:
+            return 'testing_mode'
+
+    def build_interact_task(self, url, rule):
+        """
+        构造互动数任务
+        :param url taks url
+        :param rule 互动数任务规则
+        """
+        task = {
+            'mode': HANDLER_MODE_INTERACT,                          # handler mode
+            'pid': self.task['crawlinfo'].get('pid', 0),            # project id
+            'sid': self.task['crawlinfo'].get('sid', 0),            # site id
+            'tid': self.task['crawlinfo'].get('tid', 0),            # task id
+            'uid': self.task['crawlinfo'].get('uid', 0),            # url id
+            'kid': rule['uuid'],                                    # rule id
+            'url': url,                                             # url
+            'parentid': self.task['rid'],                           # article id
+            'expire': 0 if int(rule['expire']) == 0 else int(time.time()) + int(rule['expire'])
+        }
+        self.debug("%s build interact task: %s" % (self.__class__.__name__, str(task)))
+        if not self.testing_mode:
+            '''
+            testing_mode打开时，数据不入库
+            '''
             return self.db['SpiderTaskDB'].insert(task)
         else:
             return 'testing_mode'
 
     def attach_preparse(self, rule):
+        """
+        附加任务url生成规则参数获取
+        """
         if not rule:
-            return None
+            return {}
         def build_rule(item):
             key = item.pop('key')
             if key and item['filter']:
                 if item['filter'] == '@value:parent_url':
+                    '''
+                    规则为获取父级url时，将详情页url赋给规则
+                    '''
                     item['filter'] = '@value:%s' % self.response['final_url']
                 elif item['filter'].startswith('@url:'):
+                    '''
+                    规则为@url:开头时，表示从详情页url中正则匹配数据
+                    '''
                     r = item['filter'][5:]
                     v = utils.preg(self.response['final_url'], r)
                     if not v:
-                        return None
+                        raise CDSpiderSettingError("rule: % not matched with %s" % (item['filter'], self.response['final_url']))
                     item['filter'] = '@value:%s' % v
                 return {key: item}
-            return None
+            raise CDSpiderSettingError("attach rule is invalid")
+        #格式化解析规则
         parse = {}
         if isinstance(rule, (list, tuple)):
             for item in rule:
@@ -275,8 +449,12 @@ class GeneralItemHandler(BaseHandler):
                 ret = build_rule(item)
                 if ret:
                     parse.update(ret)
-        if not parse:
-            return None
         parser = CustomParser(source=self.response['last_source'], ruleset=copy.deepcopy(parse), log_level=self.log_level, url=self.response['final_url'])
         parsed = parser.parse()
-        return utils.filter(parsed)
+        parsed = utils.filter(parsed)
+        if parsed.keys() != parse.keys():
+            '''
+            数据未完全解析到，则任务匹配失败
+            '''
+            raise CDSpiderSettingError("rule: %s not matched completion data, matched data: %s" % (str(parse), str(parsed)))
+        return parsed
