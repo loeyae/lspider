4dab00b2ecaf02960d3d61adaa2ba23916dcfe6e
Zhang Yi <loeyae@gmail.com>
2018-12-09 18:17:30 +0800

    add Comment crawl process

--- cdspider/handler/CommentHandler.py
+++ cdspider/handler/CommentHandler.py
@@ -18,40 +18,57 @@ from cdspider.parser import CustomParser
 class CommentHandler(BaseHandler):
     """
     comment handler
+    :property task 爬虫任务信息 {"mode": "comment", "uuid": SpiderTask.comment uuid}
+                   当测试该handler，数据应为 {"mode": "comment", "url": url, "detailRule": 评论规则，参考评论规则}
     """
+
     def get_scripts(self):
-        rule = self.match_rule()
-        return rule.get("scripts", None)
+        """
+        获取自定义脚本
+        """
+        try:
+            rule = self.match_rule()
+            return rule.get("scripts", None)
+        except:
+            return None
+
+    def init_process(self):
+        """
+        初始化爬虫流程
+        :output self.process {"request": 请求设置, "parse": 解析规则, "paging": 分页规则, "unique": 唯一索引规则}
+        """
+        article = self.db['ArticlesDB'].get_detail(self.task['parentid'], select=['url'])
+        if not article:
+            raise CDSpiderHandlerError("aritcle: %s not exists" % self.task['parentid'])
+        self.task['parent_url'] = article['url']
+        self.process = self.match_rule()
 
     def match_rule(self):
+        """
+        获取匹配的规则
+        """
         parse_rule = self.task.get("commentRule", {})
+        if not parse_rule:
+            '''
+            如果task中包含列表规则，则读取相应的规则，否则在数据库中查询
+            '''
+            ruleId = self.task.get('kid', 0)
+            parse_rule = self.db['CommentRuleDB'].get_detail(ruleId)
+            if not parse_rule:
+                raise CDSpiderDBDataNotFound("CommentRule: %s not exists" % self.task['kid'])
+            if rule['status'] != CommentRuleDB.STATUS_ACTIVE:
+                raise CDSpiderHandlerError("comment rule not active")
         return parse_rule
 
-    def schedule(self, message, save):
-        mode = message['mode']
-        if not 'id' in save:
-            save['id'] = 0
-        if mode == ROUTER_MODE_PROJECT:
-            if not 'sid' in save:
-                save['sid'] = 0
-            for item in self.db['SitesDB'].get_new_list(save['sid'], message['item']):
-                self.debug("%s schedule site: %s" % (self.__class__.__name__, str(item)))
-                while True:
-                    has_item = False
-                    for each in self.schedule_by_site(item, message['h-mode'], save):
-                        yield each
-                        has_item = True
-                    if not has_item:
-                        self.debug("%s schedule site end" % (self.__class__.__name__))
-                        break
-                if item['uuid'] > save['sid']:
-                    save['sid'] = item['uuid']
-        elif mode == ROUTER_MODE_SITE:
-            site = self.db['SitesDB'].get_detail(message['item'])
-            for each in self.schedule_by_site(site, message['h-mode'], save):
-                yield each
-
     def route(self, mode, save):
+        """
+        schedule 分发
+        :param mode  project|site 分发模式: 按项目|按站点
+        :param save 传递的上下文
+        :return 包含uuid的迭代器，项目模式为项目的uuid，站点模式为站点的uuid
+        :notice 该方法返回的迭代器用于router生成queue消息，以便plantask听取，消息格式为:
+        {"mode": route mode, "h-mode": handler mode, "uuid": uuid}
+        """
         if not "id" in save:
             save["id"] = 0
         if mode == ROUTER_MODE_PROJECT:
@@ -75,19 +92,140 @@ class CommentHandler(BaseHandler):
                 if item['uuid'] > save['pid']:
                     save['pid'] = item['uuid']
 
-    # 采集数据
+    def schedule(self, message, save):
+        """
+        根据router的queue消息，计划爬虫任务
+        :param message route传递过来的消息
+        :param save 传递的上下文
+        :return 包含uuid, url的字典迭代器，为SpiderTaskDB中数据
+        :notice 该方法返回的迭代器用于plantask生成queue消息，以便fetch听取，消息格式为
+        {"mode": handler mode, "uuid": SpiderTask uuid, "url": SpiderTask url}
+        """
+        mode = message['mode']
+        if not 'id' in save:
+            save['id'] = 0
+        if mode == ROUTER_MODE_PROJECT:
+            if not 'sid' in save:
+                save['sid'] = 0
+            for item in self.db['SitesDB'].get_new_list(save['sid'], message['item']):
+                self.debug("%s schedule site: %s" % (self.__class__.__name__, str(item)))
+                while True:
+                    has_item = False
+                    for each in self.schedule_by_site(item, message['h-mode'], save):
+                        yield each
+                        has_item = True
+                    if not has_item:
+                        self.debug("%s schedule site end" % (self.__class__.__name__))
+                        break
+                if item['uuid'] > save['sid']:
+                    save['sid'] = item['uuid']
+        elif mode == ROUTER_MODE_SITE:
+            site = self.db['SitesDB'].get_detail(message['item'])
+            for each in self.schedule_by_site(site, message['h-mode'], save):
+                yield each
+
+    def schedule_by_site(self, site, mode, save):
+        """
+        获取站点下计划中的爬虫任务
+        :param site 站点信息
+        :param mode handler mode
+        :param save 上下文参数
+        :return 包含爬虫任务uuid, url的字典迭代器
+        """
+        for item in self.db['SpiderTaskDB'].get_plan_list(mode, save['id'], plantime=save['now'], where={"sid": site['uuid']}, select=['uuid', 'url', 'kid']):
+            if not self.testing_mode:
+                '''
+                testing_mode打开时，数据不入库
+                '''
+                rule = self.db['CommentRuleDB'].get_detail(item.pop('kid', 0))
+                if not rule:
+                    continue
+                plantime = int(save['now']) + int(self.ratemap[str(rule.get('frequency', self.DEFAULT_RATE))][0])
+                self.db['SpiderTaskDB'].update(item['uuid'], mode, {"plantime": plantime})
+            if item['uuid'] > save['id']:
+                save['id'] = item['uuid']
+            yield item
+
     def run_parse(self, rule):
+        """
+        根据解析规则解析源码，获取相应数据
+        :param rule 解析规则
+        :input self.response 爬虫结果 {"last_source": 最后一次抓取到的源码, "final_url": 最后一次请求的url}
+        :output self.response {"parsed": 解析结果}
+        """
         parser = CustomParser(source=self.response['last_source'], ruleset=copy.deepcopy(rule), log_level=self.log_level, url=self.response['final_url'])
         self.response['parsed'] = parser.parse()
 
-    # 保存结果
     def run_result(self, save):
-        print('result')
+        """
+        爬虫结果处理
+        :param save 保存的上下文信息
+        :input self.response {"parsed": 解析结果, "final_url": 请求的url}
+        """
+        self.crawl_info['crawl_urls'][str(self.page)] = self.response['final_url']
+        self.crawl_info['crawl_count']['page'] += 1
         if self.response['parsed']:
-            typeinfo = self._typeinfo(self.response['final_url'])
-            self.result2db(save, typeinfo)
+            ctime = self.crawl_id
+            new_count = self.crawl_info['crawl_count']['new_count']
+            for each in self.response['parsed']:
+                self.crawl_info['crawl_count']['total'] += 1
+                if self.testing_mode:
+                    '''
+                    testing_mode打开时，数据不入库
+                    '''
+                    inserted, unid = (True, {"acid": "test_mode", "ctime": ctime})
+                    self.debug("%s test mode: %s" % (self.__class__.__name__, unid))
+                else:
+                    #生成唯一ID, 并判断是否已存在
+                    inserted, unid = self.db['UniqueDB'].insert(self.get_unique_setting(self.task['parent_url'], each), ctime)
+                    self.debug("%s on_result unique: %s @ %s" % (self.__class__.__name__, str(inserted), str(unid)))
+                if inserted:
+                    result = self.build_comment_info(result=each, **unid)
+                    if self.testing_mode:
+                        '''
+                        testing_mode打开时，数据不入库
+                        '''
+                        self.debug("%s result: %s" % (self.__class__.__name__, result))
+                    else:
+                        result_id = self.db['CommentsDB'].insert(result)
+                        if not result_id:
+                            raise CDSpiderDBError("Result insert failed")
+                    self.crawl_info['crawl_count']['new_count'] += 1
+                else:
+                    self.crawl_info['crawl_count']['repeat_count'] += 1
+            if self.crawl_info['crawl_count']['new_count'] - new_count == 0:
+                self.crawl_info['crawl_count']['repeat_page'] += 1
+                self.on_repetition()
 
+    def build_comment_info(self, **kwargs):
+        """
+        构造评论数据
+        """
+        result = kwargs.pop('result')
+        #爬虫信息记录
+        result['crawlinfo'] = {
+            'pid': self.task['pid'],                        # project id
+            'sid': self.task['sid'],                        # site id
+            'tid': self.task['tid'],                        # task id
+            'uid': self.task['uid'],                        # url id
+            'ruleId': self.task['kid'],                     # commentRule id
+            'rid': self.task['parentid'],                   # article rid
+            'list_url': kwargs.pop('final_url'),            # 列表url
+        }
+        result['unid'] = kwargs.pop('unid')
+        result['ctime'] = kwargs.pop('ctime')
+        return result
 
-
-
-
+    def finish(self):
+        """
+        记录抓取日志
+        """
+        super(GeneralListHandler, self).finish()
+        crawlinfo = self.task.get('crawlinfo', {}) or {}
+        self.crawl_info['crawl_end'] = int(time.time())
+        crawlinfo[str(self.crawl_id)] = self.crawl_info
+        crawlinfo_sorted = [(k, crawlinfo[k]) for k in sorted(crawlinfo.keys())]
+        if len(crawlinfo_sorted) > self.CRAWL_INFO_LIMIT_COUNT:
+            del crawlinfo_sorted[0]
+        save = self.task.get("save")
+        self.db['SpiderTaskDB'].update(self.task['uuid'], self.task['mode'], {"crawltime": self.crawl_id, "crawlinfo": dict(crawlinfo_sorted), "save": save})
