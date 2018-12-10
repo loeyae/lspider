6206da61813466addb79512faa8ff3dfaadb4240
Zhang Yi <loeyae@gmail.com>
2018-12-10 17:27:49 +0800

    add constants

--- cdspider/handler/InteractHandler.py
+++ cdspider/handler/InteractHandler.py
@@ -91,6 +91,27 @@ class InteractHandler(BaseHandler):
                         break
                 if item['uuid'] > save['pid']:
                     save['pid'] = item['uuid']
+        elif mode == ROUTER_MODE_TASK:
+            '''
+            按任务分发
+            '''
+            if not "pid" in save:
+                '''
+                初始化上下文中的pid参数,该参数用于项目数据查询
+                '''
+                save["pid"] = 0
+            for item in self.db['ProjectsDB'].get_new_list(save['pid'], select=["uuid"]):
+                while True:
+                    has_item = False
+                    for each in self.db['TaskDB'].get_new_list(save['id'], where={"pid": item['uuid']}, select=["uuid"]):
+                        has_item = True
+                        if each['uuid'] > save['id']:
+                            save['id'] = each['uuid']
+                        yield each['uuid']
+                    if not has_item:
+                        break
+                if item['uuid'] > save['pid']:
+                    save['pid'] = item['uuid']
 
     def schedule(self, message, save):
         """
@@ -105,26 +126,41 @@ class InteractHandler(BaseHandler):
         if not 'id' in save:
             save['id'] = 0
         if mode == ROUTER_MODE_PROJECT:
-            if not 'sid' in save:
-                save['sid'] = 0
-            for item in self.db['SitesDB'].get_new_list(save['sid'], message['item']):
+            if not 'tid' in save:
+                save['tid'] = 0
+            for item in self.db['TaskDB'].get_new_list(save['tid'], where={"pid": message['item']}):
                 self.debug("%s schedule site: %s" % (self.__class__.__name__, str(item)))
                 while True:
                     has_item = False
-                    for each in self.schedule_by_site(item, message['h-mode'], save):
+                    for each in self.schedule_by_task(item, message['h-mode'], save):
                         yield each
                         has_item = True
                     if not has_item:
                         self.debug("%s schedule site end" % (self.__class__.__name__))
                         break
-                if item['uuid'] > save['sid']:
-                    save['sid'] = item['uuid']
+                if item['uuid'] > save['tid']:
+                    save['tid'] = item['uuid']
         elif mode == ROUTER_MODE_SITE:
-            site = self.db['SitesDB'].get_detail(message['item'])
-            for each in self.schedule_by_site(site, message['h-mode'], save):
+            if not 'tid' in save:
+                save['tid'] = 0
+            for item in self.db['TaskDB'].get_new_list(save['tid'], where={"sid": message['item']}):
+                self.debug("%s schedule site: %s" % (self.__class__.__name__, str(item)))
+                while True:
+                    has_item = False
+                    for each in self.schedule_by_task(item, message['h-mode'], save):
+                        yield each
+                        has_item = True
+                    if not has_item:
+                        self.debug("%s schedule site end" % (self.__class__.__name__))
+                        break
+                if item['uuid'] > save['tid']:
+                    save['tid'] = item['uuid']
+        elif mode == ROUTER_MODE_TASK:
+            task = self.db['TaskDB'].get_detail(message['item'])
+            for each in self.schedule_by_task(task, message['h-mode'], save):
                 yield each
 
-    def schedule_by_site(self, site, mode, save):
+    def schedule_by_task(self, task, mode, save):
         """
         获取站点下计划中的爬虫任务
         :param site 站点信息
@@ -132,7 +168,7 @@ class InteractHandler(BaseHandler):
         :param save 上下文参数
         :return 包含爬虫任务uuid, url的字典迭代器
         """
-        for item in self.db['SpiderTaskDB'].get_plan_list(mode, save['id'], plantime=save['now'], where={"sid": site['uuid']}, select=['uuid', 'url']):
+        for item in self.db['SpiderTaskDB'].get_plan_list(mode, save['id'], plantime=save['now'], where={"tid": task['uuid']}, select=['uuid', 'url']):
             if not self.testing_mode:
                 '''
                 testing_mode打开时，数据不入库
