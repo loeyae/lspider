d3613cea2d361bf5d463eef5d1f19d59b08a33d4
Zhang Yi <loeyae@gmail.com>
2018-12-12 17:06:51 +0800

    fix bug

--- cdspider/handler/CommentHandler.py
+++ cdspider/handler/CommentHandler.py
@@ -153,7 +153,7 @@ class CommentHandler(BaseHandler):
                         yield each
                         has_item = True
                     if not has_item:
-                        self.debug("%s schedule site end" % (self.__class__.__name__))
+                        self.debug("%s schedule task end" % (self.__class__.__name__))
                         break
                 if item['uuid'] > save['tid']:
                     save['tid'] = item['uuid']
@@ -161,14 +161,14 @@ class CommentHandler(BaseHandler):
             if not 'tid' in save:
                 save['tid'] = 0
             for item in self.db['TaskDB'].get_new_list(save['tid'], where={"sid": message['item']}):
-                self.debug("%s schedule site: %s" % (self.__class__.__name__, str(item)))
+                self.debug("%s schedule task: %s" % (self.__class__.__name__, str(item)))
                 while True:
                     has_item = False
                     for each in self.schedule_by_task(item, message['h-mode'], save):
                         yield each
                         has_item = True
                     if not has_item:
-                        self.debug("%s schedule site end" % (self.__class__.__name__))
+                        self.debug("%s schedule task end" % (self.__class__.__name__))
                         break
                 if item['uuid'] > save['tid']:
                     save['tid'] = item['uuid']
