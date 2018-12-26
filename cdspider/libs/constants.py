0c162895a9364e5fd2af23b8970750136aa957e9
Zhang Yi <loeyae@gmail.com>
2018-12-26 17:27:42 +0800

    add GeneralSearchHanlder

--- cdspider/libs/constants.py
+++ cdspider/libs/constants.py
@@ -97,6 +97,8 @@ HANDLER_MODE_LINKS_CLUSTER = 'links-cluster'          # Url聚类handler
 HANDLER_MODE_WEIBO = 'weibo'                          # 微博handler
 HANDLER_MODE_COMMENT = 'comment'                      # 评论handler
 HANDLER_MODE_INTERACT = 'interact'                    # 互动数（阅读数、点赞数...）handler
+HANDLER_MODE_DEFAULT_SEARCH = 'search'                # 默认搜索handler
+HANDLER_MODE_WEIBO_SEARCH = 'weibo-search'
 
 #handler mode mapping HANDLER
 HANDLER_MODE_HANDLER_MAPPING = {
@@ -115,6 +117,8 @@ HANDLER_MODE_HANDLER_MAPPING = {
     HANDLER_MODE_WEIBO: 'WeiboHandler',
     HANDLER_MODE_COMMENT: 'CommentHandler',
     HANDLER_MODE_INTERACT: 'InteractHandler',
+    HANDLER_MODE_DEFAULT_SEARCH: 'GeneralSearchHandler',
+    HANDLER_MODE_WEIBO_SEARCH: 'WeiboSearchHandler',
 }
 
 #route mode
