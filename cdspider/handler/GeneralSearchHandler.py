15acb739818dba669947fae064eaa4db8480f300
Zhang Yi <loeyae@gmail.com>
2018-12-28 11:31:21 +0800

    optimize attachment task

--- cdspider/handler/GeneralSearchHandler.py
+++ cdspider/handler/GeneralSearchHandler.py
@@ -306,7 +306,7 @@ class GeneralSearchHandler(BaseHandler):
             if keyword['status'] != KeywordsDB.STATUS_ACTIVE:
                 self.db['SpiderTaskDB'].disable(self.task['uuid'], self.task['mode'])
                 raise CDSpiderHandlerError("keyword: %s not active" % self.task['kid'])
-            rule = self.db['AuthorListRuleDB'].get_detail_by_tid(author['tid'])
+            rule = self.db['AuthorListRuleDB'].get_detail_by_tid(self.task['tid'])
             if not rule:
                 self.db['SpiderTaskDB'].disable(self.task['uuid'], self.task['mode'])
                 raise CDSpiderDBDataNotFound("author rule by tid: %s not exists" % author['tid'])
