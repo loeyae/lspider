54ad66c804a724f440b466b026800e848e13ea09
Zhang Yi <loeyae@gmail.com>
2019-01-03 15:47:32 +0800

    optimize url_builder

--- cdspider/handler/GeneralSearchHandler.py
+++ cdspider/handler/GeneralSearchHandler.py
@@ -524,7 +524,7 @@ class GeneralSearchHandler(BaseHandler):
         formated = []
         for item in data:
             if not 'url' in item or not item['url']:
-                raise CDSpiderError("url no exists: %s @ %s" % (str(item), str(self.task)))
+                continue
             if item['url'].startswith('javascript') or item['url'] == '/':
                 continue
             try:
