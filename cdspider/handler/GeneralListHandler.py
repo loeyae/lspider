dafc2d7c0253289813b5da7f35abed94f7798f39
Zhang Yi <loeyae@gmail.com>
2019-01-21 11:16:43 +0800

    optimize

--- cdspider/handler/GeneralListHandler.py
+++ cdspider/handler/GeneralListHandler.py
@@ -275,6 +275,7 @@ class GeneralListHandler(BaseHandler):
                 raise CDSpiderHandlerError("list rule not active")
         if rule and 'jsonUrl' in rule and rule['jsonUrl']:
             self.task['url'] = rule['jsonUrl']
+            save['base_url'] = self.task['url']
 
         return rule
 
