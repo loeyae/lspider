1c340be111eac4ef96dfa99f8f28767734108057
Zhang Yi <loeyae@gmail.com>
2019-01-23 09:52:23 +0800

    optimize

--- cdspider/database/mongo/__init__.py
+++ cdspider/database/mongo/__init__.py
@@ -7,6 +7,7 @@ from .Mongo import Mongo as Base
 from .Articlesdb import ArticlesDB
 from .AttachDatadb import AttachDataDB
 from .Attachmentdb import AttachmentDB
+from .Interactdb import InteractDB
 from .ChannelRulesdb import ChannelRulesDB
 from .Commentsdb import CommentsDB
 from .CrawlLogdb import CrawlLogDB
