02e7946b57aa5e4cf903147bb1a87b08c82e5933
Zhang Yi <loeyae@gmail.com>
2019-01-23 16:17:36 +0800

    optimize

--- cdspider/handler/GeneralItemHandler.py
+++ cdspider/handler/GeneralItemHandler.py
@@ -165,7 +165,6 @@ class GeneralItemHandler(BaseHandler, NewAttachmentTask):
         r = {
             "status": kwargs.get('status', ArticlesDB.STATUS_ACTIVE),
             'url': kwargs['final_url'],
-            'mediaType': self.process.get('mediaType', self.task.get('mediaType', MEDIA_TYPE_OTHER)),
             'title': result.pop('title', None) or item.get('title', None),              # 标题
             'author': result.pop('author', None) or item.get('author', None),      # 作者
             'content': result.pop('content', None) or item.get('content', None),
