c0e8e4d2e40bbb9f18243b7eff398bdb3a82be55
zhangyi <zhangyi07@beyondsoft.com>
2019-06-25 15:21:34 +0800

    fix bug

--- cdspider/crawler/__init__.py
+++ cdspider/crawler/__init__.py
@@ -47,7 +47,7 @@ class BaseCrawler(Component):
                 "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 "
                 "Safari/537.36",
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0",
-                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                 "Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134",
                 "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; LCTE; rv:11.0) like Gecko"
     ]
